15e28bc9a5daa1e5c09f0604592a542442685d11
daffarafip24 <daffarafip24@gmail.com>
2024-02-07 15:59:09 +0700

    functional test

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -18,6 +18,11 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @GetMapping()
+    public String homepage() {
+        return "Homepage";
+    }
+
     @GetMapping("/create")
     public String createProductPage(Model model) {
         Product product = new Product();
