65f0d3a7e90bbda64f495341040cf328fd19d1e9
daffarafip24 <daffarafip24@gmail.com>
2024-03-06 08:36:14 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -6,8 +6,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OrderRepository {
-    private List<Order> orderDate = new ArrayList<>();
-    public Order save(Order order) {return null;}
-    public Order findById(String id) {return null;}
-    public List<Order> findAllByAuthor(String author) {return null;}
+    private List<Order> orderData = new ArrayList<>();
+    public Order save(Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(order.getId())) {
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i += 1;
+        }
+
+        orderData.add(order);
+        return order;
+    }
+    public Order findById(String id) {
+        for (Order savedOrder: orderData) {
+            if (savedOrder.getId().equals(id)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order savedOrder: orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 }
